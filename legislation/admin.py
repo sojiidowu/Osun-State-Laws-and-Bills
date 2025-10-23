ae88add92dae82c45716100df0e7aee6e69c0984
ayodeji <sojiidowuayodeji@gmail.com>
2025-10-23 05:30:59 +0100

    Setup Admin models

--- legislation/admin.py
+++ legislation/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Category, BillOrLaw, Payment
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ['name']
+
+
+@admin.register(BillOrLaw)
+class BillOrLawAdmin(admin.ModelAdmin):
+    list_display = ('title', 'type', 'category',
+                    'price', 'is_free')
+    list_filter = ['type', 'category', 'is_free']
+    search_fields = ['title', 'description']
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bill_or_law', 'amount',
+                    'status', 'reference')
+    list_filter = ['status']
+    search_fields = ['reference', 'user__username']
