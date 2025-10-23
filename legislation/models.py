c1436cd6d945816a1dc8928ae086bd2850482b16
ayodeji <sojiidowuayodeji@gmail.com>
2025-10-23 04:53:43 +0100

    Created category, billorlaw, payment models

--- legislation/models.py
+++ legislation/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class BillOrLaw(models.Model):
+    BILL = "BILL"
+    LAW = "LAW"
+    TYPE_CHOICES = [
+        (BILL, "Bill"),
+        (LAW, "Law"),
+    ]
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    title = models.CharField(max_length=225)
+    description = models.TextField(blank=True, null=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    file = models.FileField(upload_to="documents/")
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
+    is_free = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.type})"
+
+
+class Payment(models.Model):
+    STATUS_CHOICES = [
+        ("PENDING", "Pending"),
+        ("SUCCESS", "Success"),
+        ("FAILED", "Failed"),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bill_or_law = models.ForeignKey("BillOrLaw", on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default="PENDING")
+    payment_date = models.DateTimeField(auto_now_add=True)
+    reference = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.bill_or_law.title}"
--- osunlaw/settings.py
+++ osunlaw/settings.py
@@ -11,9 +11,26 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG") == "True"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
+    }
+}
 
 
 # Quick-start development settings - unsuitable for production
