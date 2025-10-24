8855c91fc8329c9a09ba0c84f0bc902b617624f2
ayodeji <sojiidowuayodeji@gmail.com>
2025-10-25 00:34:41 +0100

    Created law list and detail views

--- legislation/views.py
+++ legislation/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import BillOrLaw
 
-# Create your views here.
+
+def law_list(request):
+    laws = BillOrLaw.objects.all().order_by('-created_at')
+    return render(request, 'legislation/law_list.html', {'laws', laws})
+
+
+def law_detail(request, pk):
+    law = get_object_or_404(BillOrLaw, pk)
+    return render(request, 'legislation/law_detail.html', {'law', law})
